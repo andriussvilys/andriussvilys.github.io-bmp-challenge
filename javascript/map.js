1fad36feb77f71fc64b3deca5985d3cd9672b921
Your name hereAndrius <iciukas@gmail.com>
2019-07-25 15:44:01 +0200

    initial setup

---
+++ javascript/map.js
@@ -0,0 +1,171 @@
+// let map;
+function initMap() {
+
+    let styledMapType = new google.maps.StyledMapType(
+
+            [
+    {  "featureType": "administrative",
+        "elementType": "labels.text.fill",
+        "stylers": [
+            {
+                "color": "#444444"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.neighborhood",
+        "elementType": "labels.text",
+        "stylers": [
+            {
+                "lightness": "37"
+            },
+            {
+                "visibility": "on"
+            },
+            {
+                "weight": "1.00"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.land_parcel",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape",
+        "elementType": "all",
+        "stylers": [
+            {
+                "color": "#f2f2f2"
+            }
+        ]
+    },
+    {
+        "featureType": "landscape.man_made",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            },
+            {
+                "color": "#ff0000"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.park",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    },
+    {
+        "featureType": "poi.sports_complex",
+        "elementType": "geometry.fill",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "elementType": "all",
+        "stylers": [
+            {
+                "saturation": -100
+            },
+            {
+                "lightness": 45
+            }
+        ]
+    },
+    {
+        "featureType": "road.highway",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            }
+        ]
+    },
+    {
+        "featureType": "road.arterial",
+        "elementType": "labels.icon",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "elementType": "all",
+        "stylers": [
+            {
+                "visibility": "on"
+            },
+            {
+                "saturation": "-100"
+            }
+        ]
+    }
+], {name: 'simple-band-w'});
+
+    let lavington22 = {lat: 51.505182, lng: -0.0999387};
+
+    let map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13, 
+        center: lavington22,
+        disableDefaultUI: true
+    });
+
+    let greenMarker = './assets/maps/green-marker.png';
+    let marker = new google.maps.Marker({
+        position: lavington22, 
+        map: map, 
+        icon: greenMarker
+    });
+
+    map.mapTypes.set('styled_map', styledMapType);
+    map.setMapTypeId('styled_map');
+};
+
+window.eqfeed_callback = function(results) {
+  for (let i = 0; i < results.features.length; i++) {
+    let coords = results.features[i].geometry.coordinates;
+    let latLng = new google.maps.LatLng(coords[1],coords[0]);
+    let marker = new google.maps.Marker({
+      position: latLng,
+      map: map
+    });
+  }
+};
