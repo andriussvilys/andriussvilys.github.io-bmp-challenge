2d2b861ce35f2a78d086136876cca455e130c3fb
andriussvilys <iciukas@gmail.com>
2019-08-08 22:30:51 +0200

    parcel with sass, jquery, and slick -- not working

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,21 @@
+import $ from 'jquery';
+import slick from 'slick-carousel';
+
+$(document).ready(function(){
+    $('.logos-wrapper').slick({
+        rtl:true,
+        infinite:true,
+        speed: 500,
+        cssEase: 'linear',
+        // autoplay: true,
+        autoplaySpeed: 6000,
+        // arrows: true,
+        draggable: true,
+        slidesToShow: 4, 
+        slidesToScroll: 2
+    });
+  });
+
 const links = document.getElementById('links');
 
 const hamburger = document.getElementById('hamburger');
@@ -5,6 +23,8 @@ const hamburger = document.getElementById('hamburger');
 hamburger.addEventListener('click', ()=>{
     links.classList.toggle('menu-links-container__display-on')
 })
+
+
 
 // function hamburgerClick(hamburger){
 //     hamburger.addEventListener('click', ()=>{
