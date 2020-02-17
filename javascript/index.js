bfcd54a2f06bb2ab3358cf2f55ca5042e0de9d50
Your name hereAndrius <iciukas@gmail.com>
2020-02-17 17:29:08 +0100

    dots scroll along

--- javascript/index.js
+++ javascript/index.js
@@ -8,7 +8,116 @@ hamburger.addEventListener('click', ()=>{
     links.classList.toggle('menu-links-container__display-on')
 })
 
-$('.testimonials-mainBody')[0].slick({});
+let mobile = null
+
+//get a list of all partner ids in #work component
+
+
+const scrollToHorizontal = (id, parent_id) => {
+    const parent = $(`#${parent_id} .slick-dots`)[0]
+    const dot = $(`#${id}`)[0]
+    let scrollTo = {behavior: 'smooth'}
+    if(!parent){
+      return
+    }
+    if(!id){
+      scrollTo.left = 0
+      parent.scrollTo(scrollTo)
+      return
+    }
+    const dotPosition = dot.getBoundingClientRect().x
+    const parentX = parent.getBoundingClientRect().x
+        if(dot){
+        if(dotPosition - parentX > 80){
+            scrollTo.left = (dotPosition - parentX) / 2
+        }
+          else{
+              scrollTo.left = -30
+          }
+          if(parent.scrollLeft > 0){
+            scrollTo.left += parent.scrollLeft
+          }
+          parent.scrollTo(scrollTo)
+        }
+  }
+
+//   let options = {
+//     root: $('#work .slick-dots').width(),
+//     rootMargin: `0 0 0 ${$('#work .slick-dots').width()/2}px`,
+//     threshold: 1.0
+//   }
+  
+//   let observer = new IntersectionObserver(isInMiddle, options);
+//   let partnerIds = Object.values($('#work li')).map(li => li.id).filter(item => item !== undefined)
+//   partnerIds.forEach(id => {
+//     //   if(document.getElementById(id).classList.contains("slick-active")){
+//     //       console.log("is observed")
+//     //       console.log(id)
+//     //       observer.observe(document.getElementById(id))
+//     //   }
+//       document.getElementById(id).addEventListener("click", ()=>{
+//           scrollToHorizontal(id, "work")
+//       })
+//   })
+//     observer.observe()
+
+//   const isInMiddle = (entries) => {
+       
+//   }
+
+  const checkMobile = () => {
+      setTimeout(() => {          
+          if(document.getElementById("people").getBoundingClientRect() > 768){
+              return false
+          }
+          else{
+              return true
+          }
+      }, 600);
+  }
+
+  window.addEventListener("resize", ()=>{
+    mobile = checkMobile()
+  })
+  
+
+
+  $(document).on('ready', function () {
+    mobile = checkMobile()
+    setTimeout(() => {        
+        let partnerIds = Object.values($('#work li')).map(li => li.id).filter(item => item !== undefined)
+        partnerIds.forEach(id => {
+            document.getElementById(id).addEventListener("click", ()=>{
+                console.log(id)
+                scrollToHorizontal(id, "work")
+            })
+        })
+    }, 1000);
+    $(".your-class").slick({
+        dots: true,
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 3
+    });
+    $(".logos-wrapper").slick({
+        dots: true,
+        arrows: false,
+        infinite: true,
+        slidesToShow: 0.25,
+        slidesToScroll: 0.25,
+        // autoplay: true,
+        // autoplaySpeed: 600
+    });
+    $(".testimonials-mainBody").slick({
+        dots: true,
+        arrows: true,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        // autoplay: true,
+        // autoplaySpeed: 600
+    });
+});
 
 
 // const bullet2 = document.getElementById('carousel-slider--bullet_2');
